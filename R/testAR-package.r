1ac7538c5643f15ce4df612f25a20d4c21756061
Alexander D. Rowe <adrowe1@gmail.com>
2013-11-12 22:18:00 +0100

    now?

--- R/testAR-package.r
+++ R/testAR-package.r
@@ -20,41 +20,48 @@ qqplot.data <- function (vec)
 
 
 
+compare.models <- function ( raw ) {
+      dat <- reshape2:::melt.data.frame(raw, id.vars=c("type", "time"), variable.name="measurement", value.name="uptake")
 
+    #ggplot(dat, aes(x=time*2, y=uptake, colour=type) ) + geom_point() + geom_smooth(method="lm")
 
-data('raw')
+    model.minimal <- lm(uptake ~time, dat)
+    model <- lm(uptake ~time * type, dat)
 
-dat <- reshape2:::melt.data.frame(raw, id.vars=c("type", "time"), variable.name="measurement", value.name="uptake")
+    #summary(model)
 
-ggplot(dat, aes(x=time*2, y=uptake, colour=type) ) + geom_point() + geom_smooth(method="lm")
+    # qqplot.data(model$res)
 
-model.minimal <- lm(uptake ~time, dat)
-model <- lm(uptake ~time * type, dat)
-
-summary(model)
-
-# qqplot.data(model$res)
-
-anova(model.minimal, model)
-
-dat$pred <- predict(model, dat)
-dat$residuals <- dat$uptake-dat$pred
+    anova(model.minimal, model)
+}
 
 
 
 
-ggplot(dat, aes(x=residuals, fill=type)) + geom_histogram(binwidth=3, position="dodge")
-plotit <- ggplot(dat, aes(x=time*2, y=residuals, colour=type) ) + geom_point() + geom_smooth()
+#dat$pred <- predict(model, dat)
+#dat$residuals <- dat$uptake-dat$pred
 
-dat$res.class <- "mid"
-dat$res.class[dat$residuals>5] <- "high"
-dat$res.class[dat$residuals<(-3)]<- "low"
+ 
+plotit <- function ( raw ) {
+        dat <- reshape2:::melt.data.frame(raw, id.vars=c("type", "time"), variable.name="measurement", value.name="uptake")              
+        
+        # ggplot(dat, aes(x=residuals, fill=type)) + geom_histogram(binwidth=3, position="dodge")
+        ggplot(dat, aes(x=time*2, y=residuals, colour=type) ) + geom_point() + geom_smooth()
+        
+        
+}
 
 
-model2 <- lm(uptake ~ time * type, dat[dat$time>4,])
-model3 <- lm(uptake ~ time, dat[dat$time>4,])
-anova(model2, model3)
 
+# dat$res.class <- "mid"
+# dat$res.class[dat$residuals>5] <- "high"
+# dat$res.class[dat$residuals<(-3)]<- "low"
+# 
+# 
+# model2 <- lm(uptake ~ time * type, dat[dat$time>4,])
+# model3 <- lm(uptake ~ time, dat[dat$time>4,])
+# anova(model2, model3)
+# 
 # qqnorm(model2$res)
 # qqnorm(model3$res)
 
