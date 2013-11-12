8c3acabbfba9c1dd3f91e214131323e7f20b68db
Alexander D. Rowe <adrowe1@gmail.com>
2013-11-12 20:33:17 +0100

    again

--- R/testAR-package.r
+++ R/testAR-package.r
@@ -21,7 +21,7 @@ qqplot.data <- function (vec)
 
 
 
-data("dat.rdata")
+data("data/dat.rdata")
 
 dat <- reshape2:::melt.data.frame(raw, id.vars=c("type", "time"), variable.name="measurement", value.name="uptake")
 
